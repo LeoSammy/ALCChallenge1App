5d8ae127f3f3d71ac60f187ae5d866b0153819eb
LeoSammy <leosammy6@gmail.com>
2019-07-15 04:24:38 +0100

    Initial commit

---
+++ app/src/main/java/com/example/alcchallenge1app/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.alcchallenge1app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /*  Button Methods    */
+    //Call onClickAbout() when this button1 is clicked
+    public void onClickAbout(View v){
+        Intent intent1 = new Intent(this, AboutActivity.class);
+        startActivity(intent1);
+    }
+    //Call onClickProfile() when this button2 is clicked
+    public void onClickProfile(View v){
+        Intent intent2 = new Intent(this, ProfileActivity.class);
+        startActivity(intent2);
+    }
+}
+
+
