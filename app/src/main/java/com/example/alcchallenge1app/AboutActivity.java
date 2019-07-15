125c610ee7a334c926a59d5a89e1f697079a2a0c
LeoSammy <leosammy6@gmail.com>
2019-07-15 05:05:27 +0100

    added the webview client and updated manifest file to turn on internet permisions for app

--- app/src/main/java/com/example/alcchallenge1app/AboutActivity.java
+++ app/src/main/java/com/example/alcchallenge1app/AboutActivity.java
@@ -2,12 +2,19 @@ package com.example.alcchallenge1app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class AboutActivity extends AppCompatActivity {
-
+    private WebView myWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
+        myWebView = findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://andela.com/alc/");
         setContentView(R.layout.activity_about_alc);
+        super.onCreate(savedInstanceState);
     }
 }
